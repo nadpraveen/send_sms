32fd4174b0209b88e7c9ec0ed64181ff2b2ff671
Praveen Kumar <praveen.42000@gmail.com>
2022-09-07 00:20:09 +0530

    first commit

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+namespace techunico\sms_sender;
+
+class sms_sender{
+    public function __construct() {
+        
+    }
+    
+    public function sender($message,$contact){
+        
+    }
+}
+
